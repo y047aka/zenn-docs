a9219a2db86d4cb90e56633136d829e73a8359ea
Yoshitaka Totsuka <y047aka@gmail.com>
2026-01-12 12:40:49 +0900

    env: nix develop

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Zenn CLI environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.zenn-cli ];
+        };
+      }
+    );
+}
